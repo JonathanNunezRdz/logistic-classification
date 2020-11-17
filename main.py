6d92ee83fb7b5c5121e5995d7efbd5527d7dc50d
JonathanNunezRdz <jonathannunezr1@gmail.com>
2020-11-17 01:04:47 -0600

    Refactored

--- main.py
+++ main.py
@@ -11,89 +11,73 @@
 def main():
     #   import standard libraries
     import numpy as np
-    import pandas as pd
 
     #   import functions
-    import utilityFunctions as uf
-    import sys
-    import time
-
-    print("--- the program has started, please wait ---")
-    start_time = time.time()
+    import utilityFunctions as uf    
 
     #   define the name of the flie to read the data from
-    csv = 'diabetes.csv'
+    csv = "diabetes.csv"
     #   define the training data size, testing data size will be calculated accordingly
     training_size = 0.8
     #   define whether to plot the w values at the end of the program
     create_histogram = True
 
-    #   display is required and is used for whether or not to print the training data set:
-    #
-    #   display = 1, will print to the console
-    #   display = 0, will not print
-    display = 0
+    #   display is required and is used for whether or not to print the Particular Explanatory Data Analysis:
+    display = False
     #   -------------------------------------------------------------------------------------------
     #   will_scale is required and it will define whether or not to implement feature 
     #   scalling for the testing data 
-    # 
-    #   will_scale = 1, will implement feature scalling 
-    #   wii_scale = 0, will not
-    will_scale_x = 1
+    will_scale_x = True
     #   -------------------------------------------------------------------------------------------    # 
     #   uf.load_data will read the determined csv (must be local) with pandas and divide tha data to x_training 
-    #   and y_training, the data will be divided in 80% to training and 20% to testing which will be used to 
-    #   calculate the gradient of cost function.
-    #
-    #   it will also return the mean and deviation to use it to predict
-    x_training, y_training, x_testing, y_testing, labels = uf.load_data(csv, display, will_scale_x, training_size)
+    #   and y_training, the data will be divided as specified in "training size" in line 25.
+    x_training, y_training, x_testing, y_testing, labels = uf.load_data(csv, will_scale_x, training_size, display)
 
-
-    #   declare w to zeros, w will be the "weight" for each of the features in the data set, which will then be 
-    #   calculated with the gradient descent method
-    w = np.zeros([x_training.shape[1], 1])
     #   -------------------------------------------------------------------------------------------    
     #   display is optional and is used for whether or not to print w and L2_norm for each iteration in gradient descent
     #   WARNING!! --> printing will impact heavily the time it takes to calculate w, for low end machines leave it set 
-    #                 equal to 0
-    #
-    #   display = 1, will print to the console
-    #   display = 0, will not print
-    display = 0
+    #                 to False
+    display = False
     #   -------------------------------------------------------------------------------------------
-    #   stopping_criteria is used for stopping the iterations, the gradient of cost will calculate L2_norm each itaration
+    #   stopping_criteria is used for stopping the iterations, the gradient of cost will calculate L2_norm each
+    #   itaration
     #   and will stop when it reaches the stopping_criteria
     #
     #   learning_rate is used for lower the "force" of the gradient of cost, it is recommended to start at a low value,
-    #   otherwise when it starts to "correct/update" the w values, there is a chance that it will go beyond the minimum and will
+    #   otherwise when it starts to "correct/update" the w values, there is a chance that it will go beyond the minimum
+    #   and will
     #   try to correct again (there's also a chance that it will stay in an infinite loop)
     stopping_criteria = 0.01
-    learning_rate = 0.0005
-    #   -------------------------------------------------------------------------------------------
-    #   calculate w values with the gradient descent method
-    w, features_histogram = uf.gradient_descent_multivariate(x_training, y_training, w, stopping_criteria, learning_rate, display, create_histogram)
+    learning_rates = [0.0005,0.001,0.005,0.01,0.05,0.1,0.5]
 
+    for learning_rate in reversed(learning_rates):
+        uf.print_title("Using {} as learning rate".format(learning_rate))
 
-    #   make the predictions based on the test data (with feature scaling)
-    y = uf.predict(w,x_testing)
-    #   -------------------------------------------------------------------------------------------
-    #   print_results takes as arguments the array of shape(n,1), the name of the rows, and the title to print before the values
-    uf.print_results(w, 'w', 'w parameters')
-    uf.print_results(y, 'y', 'outcome [prediction]', predictions=1)
+        #   declare w to zeros, w will be the "weight" for each of the features in the data set, which will then be 
+        #   calculated with the gradient descent method
+        w = np.zeros([x_training.shape[1], 1])
+        #   -------------------------------------------------------------------------------------------
+        #   calculate w values with the gradient descent method
+        w, features_histogram = uf.gradient_descent_multivariate(x_training, y_training, w, stopping_criteria, learning_rate, display, create_histogram)
 
+        #   make the predictions based on the test data (with feature scaling)
+        y = uf.predict(w,x_testing)
+        #   -------------------------------------------------------------------------------------------
+        #   print_results takes as arguments the array of shape(n,1), the name of the rows, and the title to print before
+        #   the values
+        uf.print_results(w, 'w', 'w parameters')
+        uf.print_results(y, 'y', 'outcome [prediction]', predictions=True)
 
-    #   get_confusion_matrix will return a matrix with the true positives, true negatives, false positives, and false negatives
-    #   -------------------------------------------------------------------------------------------
-    confusion_matrix = uf.get_confussion_matrix(y,y_testing)
-    #   print_performance_metrics will print the accuracy, precision, recall, specificity, and f1 score based on the confusion matrix
-    #   -------------------------------------------------------------------------------------------
-    uf.print_performance_metrics(confusion_matrix)
+        #   get_confusion_matrix will return a matrix with the true positives, true negatives, false positives, and false
+        #   negatives
+        #   -------------------------------------------------------------------------------------------
+        confusion_matrix = uf.get_confussion_matrix(y,y_testing)
+        #   print_performance_metrics will print the accuracy, precision, recall, specificity, and f1 score based on the
+        #   confusion matrix
+        #   -------------------------------------------------------------------------------------------
+        uf.print_performance_metrics(confusion_matrix)
 
-    print("--- the program has ended ---")
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    if create_histogram: uf.create_histogram(features_histogram, labels)
-
+        if create_histogram: uf.create_histogram(features_histogram, labels)
 
 if __name__ == "__main__":
     main()
--- utilityFunctions.py
+++ utilityFunctions.py
@@ -4,6 +4,7 @@
     predicting.
 
     Author:         Jonathan Nunez Rdz.
+                    Ezequiel
     Institution:    Universidad de Monterrey
     First Created:  Sat 18 April, 2020
     Email:          jonathan.nunez@udem.edu // jonathannunezr1@gmail.com
@@ -13,32 +14,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
-import sys
+import time
+import itertools
 
 """load data from input csv by user"""
-def load_data(file, display, will_scale_x, training_size):
+def load_data(file:str, will_scale_x:bool, training_size:float, display:bool):
     try:
         data = pd.read_csv(file)
     except:
-        print("-"*40)
-        print("'./{}' file directory doesn't exist".format(file))
-        print("-"*40)
+        print_title("'./{}' file directory doesn't exist".format(file))
         exit(1)
     
     # split data into training and testing
     labels = data.columns.values
     training_data = data.sample(frac=training_size)
     testing_data = data.drop(training_data.index)
-
-    if(display == 1):
-        print('-'*40)
-        print("training data set")
-        print('-'*40)
-        print(training_data)
-        print('-'*40)
-        print("testing data set")
-        print('-'*40)
-        print(testing_data)
 
     # split training data into x's and y
     x_training = pd.DataFrame.to_numpy(training_data.iloc[:, 0:-1])
@@ -48,28 +38,24 @@ def load_data(file, display, will_scale_x, training_size):
     x_testing = pd.DataFrame.to_numpy(testing_data.iloc[: , 0:-1])
     y_testing = pd.Series.to_numpy(testing_data.iloc[:, -1]).reshape(x_testing.shape[0], 1)
 
+    if display:
+        print_title('Particular Explratory Data Analysis')
+        print_subtitle('10 Training data samples')
+        for i in range(10): print('\t'.join([np.format_float_positional(j,3) for j in x_training[i,:]]))
+        print_subtitle('10 Testing data samples')
+        for i in range(10): print('\t'.join([np.format_float_positional(j,3) for j in x_testing[i,:]]))
+        print_statistics(x_training, 'Training')    
+        print_statistics(x_testing, 'Testing')    
+
     # feature scalling for x if specified
     ones = np.ones([x_training.shape[0],1])
-    if(will_scale_x == 1):
+    if will_scale_x:
         # feature scalling for training
         x_training_scaled, mean, deviation = scale_x(x_training, 'training')
         x_training_scaled = np.concatenate((ones,x_training_scaled), axis=1)
 
         # feature scalling for testing
-        x_testing_scaled = scale_x(x_testing, 'testing', mean=mean, deviation=deviation)       
-
-        # display option
-        if(display == 1):
-            print('-'*40)
-            print("training features scaled")
-            print('-'*40)
-            for i in range(x_training_scaled.shape[0]):
-                print(x_training_scaled[i])
-            print('-'*40)
-            print("testing features scaled")
-            print('-'*40)
-            for i in range(x_testing_scaled.shape[0]):
-                print(x_testing_scaled[i])
+        x_testing_scaled = scale_x(x_testing, 'testing', mean=mean, deviation=deviation)
 
         # return the training data and testing data scaled
         return x_training_scaled, y_training, x_testing_scaled, y_testing, labels
@@ -82,84 +68,61 @@ def load_data(file, display, will_scale_x, training_size):
 def scale_x(x, mode, **kwargs):
     if(mode == 'training'):
         x_scaled = np.zeros_like(x)
-        mean = np.zeros(shape = (x_scaled.shape[1], 1))
-        deviation = np.zeros(shape = (x_scaled.shape[1], 1))
-
+        mean = np.zeros([1, x_scaled.shape[1]])
+        deviation = np.zeros([1, x_scaled.shape[1]])
         for i in range(x.shape[1]):
-
-            col = np.zeros_like(x)
-
-            for j in range(x.shape[0]):
-                col[j] = x[j][i]
-            
-            mean[i] = col.mean()
-            deviation[i] = col.std()
-
-            for j in range(x.shape[0]):
-                x_scaled[j][i] = (x[j][i] - col.mean()) / col.std()
-
-        return x_scaled,mean,deviation
-    
+            mean[0,i] = x[:,i].mean()
+            deviation[0,i] = x[:,i].std()
+            for j in range(x.shape[0]): x_scaled[j,i] = (x[j,i] - mean[0,i]) / np.sqrt(deviation[0,i]**2+10**-8)
+        return x_scaled,mean,deviation    
     if(mode == 'testing'):
         defined_mean = kwargs.get('mean')
         defined_deviation = kwargs.get('deviation')
         x_scaled = np.zeros_like(x)
-
         for i in range(x.shape[1]):
-            for j in range(x.shape[0]):
-                x_scaled[j][i] = (x[j][i] - defined_mean[i]) / defined_deviation[i]
-        
+            for j in range(x.shape[0]): x_scaled[j,i] = (x[j,i] - defined_mean[0,i]) / np.sqrt(defined_deviation[0,i]**2+10**-8)
         return x_scaled
 
 """calculate w until the L2_norm reaches the stopping criteria"""
-def gradient_descent_multivariate(x_training, y_training, w, stopping_criteria, learning_rate, display, create_histogram):
-    iteration = 0
+def gradient_descent_multivariate(x_training, y_training, w, stopping_criteria:float, learning_rate:float, display:bool, create_histogram:bool):
+    print("--- the gradient descent has started, please wait ---")
+    start_time = time.time()
+    L2_norm = 100.0
+    iterations = 0
+    cost_function_histogram = np.zeros([0,w.shape[0]])
     if create_histogram: 
-        features_histogram = np.zeros([0,w.shape[0]])
-
-        L2_norm = 100.0
-        while L2_norm > stopping_criteria:
-            features_histogram = np.append(features_histogram, w.T, axis=0)
-
+        for i in itertools.count():
+            if L2_norm <= stopping_criteria:
+                iterations = i
+                break
             gradient_of_cost_function = compute_gradient_of_cost_function_multivariate(x_training, y_training, w)
+            cost_function_histogram = np.append(cost_function_histogram, gradient_of_cost_function.T, axis=0)
             w = w - (learning_rate * gradient_of_cost_function)
             L2_norm = compute_L2_norm_multivariate(gradient_of_cost_function)
-            iteration += 1
-
-            if(display == 1):
-                print('w: {}, L2: {}'.format(w, L2_norm))
-
-        print("--- {} L2_norm ---".format(L2_norm))
-        print("--- {} iterations ---".format(iteration))
-        return w, features_histogram
+            if display: print('w: {}, L2: {}'.format(w, L2_norm))
     else:
-        L2_norm = 100.0
-        while L2_norm > stopping_criteria:
+        for i in itertools.count():
+            if L2_norm <= stopping_criteria:
+                iterations = i
+                break            
             gradient_of_cost_function = compute_gradient_of_cost_function_multivariate(x_training, y_training, w)
             w = w - (learning_rate * gradient_of_cost_function)
             L2_norm = compute_L2_norm_multivariate(gradient_of_cost_function)
-            iteration += 1
-
-            if(display == 1):
-                print('w: {}, L2: {}'.format(w, L2_norm))
-
-        print("--- {} L2_norm ---".format(L2_norm))
-        print("--- {} iterations ---".format(iteration))
-        return w, 0
+            if display: print('w: {}, L2: {}'.format(w, L2_norm))        
+    seconds = time.time() - start_time
+    print("--- the gradient descent has ended, see resuls ---")
+    print("--- {} seconds ---".format(seconds))
+    print("--- {} iterations ---".format(iterations))
+    if create_histogram: return w, cost_function_histogram
+    return w, 0
 
 """compute the cost of the gradient"""
 def compute_gradient_of_cost_function_multivariate(x,y,w):
-
     N = x.shape[0]
-
     hypothesis_function = eval_hypothesis_function_multivariate(w, x)
-
     residual = np.subtract(hypothesis_function, y.T)
-
     gradient_of_cost_function = ((residual * x.T).sum(axis=1)/N)
-
     gradient_of_cost_function = np.reshape(gradient_of_cost_function, (gradient_of_cost_function.shape[0], 1))
-
     return gradient_of_cost_function
 
 """get hypothesis function with w and x"""
@@ -172,41 +135,31 @@ def compute_L2_norm_multivariate(gradient_of_cost_function):
 
 """print results"""
 def print_results(results, name, title, **kwargs):
-    print('-'*40)
-    print(title)
-    print('-'*40)
-    if(kwargs.get('predictions') == 1):
+    print_title(title)
+    if kwargs.get('predictions'):
         with_diabetes = 0
         without_diabetes = 0
         for i in range(results.shape[0]):
-            print("{}[{}]: {} => {}".format(name,i,results[i][0],results[i][1]))
-            if(results[i][1] == 1):
-                with_diabetes += 1
-            else:
-                without_diabetes += 1
+            print("{}[{}]: {}\t=> {}".format(name,i,results[i,0],results[i,1]))
+            if results[i,1] == 1: with_diabetes += 1
+            else: without_diabetes += 1
         print("with diabetes\t\t=> {}".format(with_diabetes))
         print("without diabetes\t=> {}".format(without_diabetes))
-    else:
-        for i in range(results.shape[0]):
-            print("{}[{}]: {}".format(name,i,results[i][0]))
+    else: 
+        for i in range(results.shape[0]): print("{}[{}]: {}".format(name,i,results[i,0]))
 
 """predict the y values for a x_test input"""
 def predict(w, x):
     y = np.zeros([x.shape[0],1])
-
     for i in range(x.shape[0]):   
         y[i] += w[0]      
         for j in range(1,w.shape[0]):
-            y[i] += w[j]*x[i][j-1]
-
+            y[i] += w[j]*x[i,j-1]
     new_y = np.zeros([x.shape[0],2])
     for i in range(new_y.shape[0]):
-            new_y[i][0] = 1/(1 + math.exp(-y[i]))
-            if(new_y[i][0] >= 0.5):
-                new_y[i][1] = 1
-            else:
-                new_y[i][1] = 0
-
+            new_y[i,0] = 1/(1 + math.exp(-y[i,0]))
+            if new_y[i,0] >= 0.5: new_y[i,1] = 1
+            else: new_y[i,1] = 0
     return new_y
 
 """calculate the confusion matrix with the given predicted class and the actual class"""
@@ -218,24 +171,24 @@ def get_confussion_matrix(predicted_class, actual_class):
     FN = 0
 
     for i in range(predicted_class.shape[0]):
-        if(predicted_class[i][1] == 1 and actual_class[i][0] == 1): TP += 1
-        elif (predicted_class[i][1] == 0 and actual_class[i][0] == 0): TN += 1
-        elif (predicted_class[i][1] == 1 and actual_class[i][0] == 0): FP += 1
+        if(predicted_class[i,1] == 1 and actual_class[i,0] == 1): TP += 1
+        elif (predicted_class[i,1] == 0 and actual_class[i,0] == 0): TN += 1
+        elif (predicted_class[i,1] == 1 and actual_class[i,0] == 0): FP += 1
         else: FN += 1
 
-    confusion_matrix[0][0] = TP
-    confusion_matrix[1][1] = TN
-    confusion_matrix[0][1] = FP
-    confusion_matrix[1][0] = FN
+    confusion_matrix[0,0] = TP
+    confusion_matrix[1,1] = TN
+    confusion_matrix[0,1] = FP
+    confusion_matrix[1,0] = FN
 
     return confusion_matrix
 
 """print to the console the performance metrics"""
 def print_performance_metrics(confusion_matrix):
-    TP = confusion_matrix[0][0]
-    TN = confusion_matrix[1][1]
-    FP = confusion_matrix[0][1]
-    FN = confusion_matrix[1][0]
+    TP = confusion_matrix[0,0]
+    TN = confusion_matrix[1,1]
+    FP = confusion_matrix[0,1]
+    FN = confusion_matrix[1,0]
 
     accuracy = (TP + TN)/(TP + TN + FP + FN)
     precision = TP /(TP + FP)
@@ -243,16 +196,12 @@ def print_performance_metrics(confusion_matrix):
     specificity = TN/(TN + FP)
     f1_score = 2*(precision * recall)/(precision + recall)
 
-    print('-'*40)
-    print('confusion matrix')
-    print('-'*40)
+    print_title("confusion matrix")
     print('true positives\t\t=> {}'.format(TP))
     print('true negatives\t\t=> {}'.format(TN))
     print('false positives\t\t=> {}'.format(FP))
     print('false negatives\t\t=> {}'.format(FN))
-    print('-'*40)
-    print('scores')
-    print('-'*40)
+    print_title("scores")
     print('accuracy\t\t=> {}'.format(accuracy))
     print('precision\t\t=> {}'.format(precision))
     print('recall\t\t\t=> {}'.format(recall))
@@ -262,13 +211,41 @@ def print_performance_metrics(confusion_matrix):
 """create a plot with the features and their values across the interation number"""
 def create_histogram(features_histogram, labels):
     plt.subplot(111)
-    plt.xlabel('# of iterations')
-    plt.ylabel("value of w's")
-    plt.title('feature histogram')
+    plt.xlabel('# of iteration')
+    plt.ylabel("Cost")
+    plt.title('Cost of function histogram')
 
     x_plot = np.arange(features_histogram.shape[0])
     for i in range(features_histogram.shape[1]):
         plt.plot(x_plot, np.array(features_histogram[:,i]), label=labels[i])
     
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.legend(loc='lower right')
+    plt.grid()
     plt.show()
+
+def print_title(title:str):
+    """
+        print a title with a format
+    """
+    print("-"*40)
+    print(title)
+    print("-"*40)
+
+def print_subtitle(subtitle:str, **kwargs):
+    """
+        print a subtitle with a format
+    """
+    print("-"*20)
+    if kwargs.get('extra') != None: print(subtitle, kwargs.get('extra'))
+    else: print(subtitle)
+    print("-"*20)
+
+def print_statistics(data, title):
+    """
+        print a subtitle with a format
+    """
+    print_title('Statistics - {}'.format(title))
+    print_subtitle('Min (per feature)    =>', extra=[data[:,i].min() for i in range(data.shape[1])])
+    print_subtitle('Max (per feature)    =>', extra=[data[:,i].max() for i in range(data.shape[1])])
+    print_subtitle('Mean (per feature)   =>', extra=[float(np.format_float_positional(data[:,i].mean(), 3)) for i in range(data.shape[1])])
+    print_subtitle('Median (per feature) =>', extra=[float(np.format_float_positional(np.median(data[:,i]))) for i in range(data.shape[1])])
